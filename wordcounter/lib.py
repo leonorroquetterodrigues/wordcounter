5d6737a0e4b4f18866ddedc549c04c6bc5e79c88
leonor rodrigues <leonorroquetterodrigues@gmail.com>
2020-11-09 16:19:00 +0000

    update

--- wordcounter/lib.py
+++ wordcounter/lib.py
@@ -2,56 +2,46 @@
 # Copyright (C) 2018 Jean Bizot <jean@styckr.io>
 """ Main lib for wordcounter Project
 """
-
-from os.path import split
+import nltk
+#from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer
+#nltk.download('nps_chat')
+from nltk.corpus import nps_chat
+#nltk.download('wordnet')
+# Lemmatize with POS Tag
+from nltk.corpus import wordnet
 import pandas as pd
-import datetime
 
-pd.set_option('display.width', 200)
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
 
 
-def clean_data(data):
-    """ clean data
-    """
-    # Remove columns starts with vote
-    cols = [x for x in data.columns if x.find('vote') >= 0]
-    data.drop(cols, axis=1, inplace=True)
-    # Remove special characteres from columns
-    data.loc[:, 'civility'] = data['civility'].replace('\.', '', regex=True)
-    # Calculate Age from day of birth
-    actual_year = datetime.datetime.now().year
-    data.loc[:, 'Year_Month'] = pd.to_datetime(data.birthdate)
-    data.loc[:, 'Age'] = actual_year - data['Year_Month'].dt.year
-    # Uppercase variable to avoid duplicates
-    data.loc[:, 'city'] = data['city'].str.upper()
-    # Take 2 first digits, 2700 -> 02700 so first two are region
-    data.loc[:, 'postal_code'] = data.postal_code.str.zfill(5).str[0:2]
-    # Remove columns with more than 50% of nans
-    cnans = data.shape[0] / 2
-    data = data.dropna(thresh=cnans, axis=1)
-    # Remove rows with more than 50% of nans
-    rnans = data.shape[1] / 2
-    data = data.dropna(thresh=rnans, axis=0)
-    # Discretize based on quantiles
-    data.loc[:, 'duration'] = pd.qcut(data['surveyduration'], 10)
-    # Discretize based on values
-    data.loc[:, 'Age'] = pd.cut(data['Age'], 10)
-    # Rename columns
-    data.rename(columns={'q1': 'Frequency'}, inplace=True)
-    # Transform type of columns
-    data.loc[:, 'Frequency'] = data['Frequency'].astype(int)
-    # Rename values in rows
-    drows = {1: 'Manytimes', 2: 'Onetimebyday', 3: '5/6timesforweek',
-             4: '4timesforweek', 5: '1/3timesforweek', 6: '1timeformonth',
-             7: '1/trimestre', 8: 'Less', 9: 'Never'}
-    data.loc[:, 'Frequency'] = data['Frequency'].map(drows)
-    return data
+def part_of_speech(sentence):
+    chat_tagged = nps_chat.tagged_words(tagset="universal")
+    tuple_list = chat_tagged
+    word = []
+    category = []
+    for a_tuple in tuple_list:
+        word.append(a_tuple[0])
+    for a_tuple in tuple_list:
+        category.append(a_tuple[1])
+    df = pd.DataFrame(zip(word,category),columns=['word', 'category'])
+    df_unique = df.drop_duplicates(subset=['word'])# subset=['word'] -> if we want to get the first  category for each word
+    lemmatizer = WordNetLemmatizer()
+    aux = [lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in nltk.word_tokenize(sentence.lower())]
+    aux_1 = pd.DataFrame(aux,columns=['word'])
+    aux_2 = pd.DataFrame(aux_1.word.value_counts().index,columns=['word'])
+    return pd.merge(df_unique, aux_2, on=['word'],how='inner', validate='1:1')
 
 
 if __name__ == '__main__':
-    # For introspections purpose to quickly get this functions on ipython
-    import wordcounter
-    folder_source, _ = split(wordcounter.__file__)
-    df = pd.read_csv('{}/data/data.csv.gz'.format(folder_source))
-    clean_data = clean_data(df)
-    print(' dataframe cleaned')
+   print('this are my functions get_wordnet_pos and part_of_speech')
+
+
